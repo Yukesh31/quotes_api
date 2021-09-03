349eaa3b5c27a41f5139c87311b2addcba1461db
Yukesh31 <vyukeshkumaran@gmail.com>
2021-09-03 21:39:21 +0530

    second

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+let text = document.querySelector("#quote");
+let author = document.querySelector("#author");
+
+const getNewQuote = async () => {
+  let url = "https://type.fit/api/quotes";
+  const data = await fetch(url); 
+  //fetching quotes
+  const quotes = await data.json();
+   //converting json to object
+  
+  const index = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[index].text;
+  const authr = quotes[index].author;
+  if (authr == null) {
+    author = "Anonymous";
+  }
+  text.innerHTML = quote;
+  author.innerHTML = `- ${authr}`;
+};
+getNewQuote();
